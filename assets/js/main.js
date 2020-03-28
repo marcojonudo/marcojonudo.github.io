a2035803e140410d8c8d1f9aeee620b281adf0e6
Marco <marco.martinez@feuvert.es>
2020-03-28 22:56:28 +0100

    Base structure with mousemove header animation

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+const windowWidth = $(window).width();
+const windowHeight = $(window).height();
+
+$('#header').mousemove((event) => {
+    const moveX = ((windowWidth / 2) - event.pageX) * 0.01;
+    const moveY = ((windowHeight / 2) - event.pageY) * 0.01;
+
+    $('#header-image').css('margin-left', `${moveX}px`);
+    $('#header-image').css('margin-top', `${moveY}px`);
+});
