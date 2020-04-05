2b7cb80b9ec8315adb2a00be33183812f33dfa94
Marco <marco.martinez@feuvert.es>
2020-04-05 13:54:24 +0200

    Update header design

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,13 @@
 const windowWidth = $(window).width();
 const windowHeight = $(window).height();
 
-$('#header').mousemove((event) => {
-    const moveX = ((windowWidth / 2) - event.pageX) * 0.01;
-    const moveY = ((windowHeight / 2) - event.pageY) * 0.01;
+// $('#header').mousemove((event) => {
+//     const moveX = ((windowWidth / 2) - event.pageX) * 0.01;
+//     const moveY = ((windowHeight / 2) - event.pageY) * 0.01;
 
-    $('#header-image').css('margin-left', `${moveX}px`);
-    $('#header-image').css('margin-top', `${moveY}px`);
-});
+//     $('#header-image').css('margin-left', `${moveX}px`);
+//     $('#header-image').css('margin-top', `${moveY}px`);
+// });
 
 $(window).on('load', () => {
     setTimeout(() => {
